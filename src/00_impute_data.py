53e63009325a81d18450c2b0006fc10572fcb673
Pepa Aran Paredes <pepa@pop-os.localdomain>
2023-11-08 12:23:29 +0100

    comment and fix paths

--- src/00_impute_data.py
+++ src/00_impute_data.py
@@ -1,13 +1,21 @@
+# This script loads the raw dataset, removes unnecessary columns, imputes missing values using K-nearest neighbors
+# for specific columns, and then saves the cleaned and imputed dataset to a new CSV file named 'df_imputed.csv'.
+
+# Load dependencies
 from sklearn.impute import KNNImputer
 import pandas as pd
 
-data = pd.read_csv('../data/df_20210510.csv', index_col=0).drop(columns=['lat', 'lon', 'elv','c4','whc','LE_F_MDS','NEE_VUT_REF'])
+# Load the raw dataset from a CSV file and remove unnecessary columns
+# about site characteristics and net ecosystem fluxes (too correlated with GPP)
+data = pd.read_csv('../data/raw/df_20210510.csv', index_col=0).drop(columns=['lat', 'lon', 'elv','c4','whc','LE_F_MDS','NEE_VUT_REF'])
 
-data = data[data.index != 'CN-Cng'] # missing meta-data info for this site
+# Remove data for the site 'CN-Cng' due to missing meta-data
+data = data[data.index != 'CN-Cng']
 
 sites = data.index.unique()
 
-# Impute TA_F_DAY (with TA_F and SW_IN_F)
+# Impute 'TA_F_DAY' and 'TA_F_NIGHT' columns using 'TA_F' and 'SW_IN_F'
+# Iterate over sites to perform imputation for each site
 df =  data[['TA_F','SW_IN_F','TA_F_DAY', 'TA_F_NIGHT']]
 for s in sites:
     impute = KNNImputer()
@@ -16,12 +24,14 @@ for s in sites:
     data.loc[data.index == s, 'TA_F_DAY'] = x[:,2]
     data.loc[data.index == s, 'TA_F_NIGHT'] = x[:,3]
 
-# Impute GPP (with SW_IN_F, LW_IN_F, TA_F, WS_F, VPD_F, P_F, TA_F_DAY)
+# Impute 'GPP_NT_VUT_REF' column using multiple, selected features
+# Iterate over sites to perform imputation for each site
 df =  data[['TA_F','SW_IN_F','TA_F_DAY', 'LW_IN_F','WS_F','P_F', 'VPD_F', 'GPP_NT_VUT_REF']]
 for s in sites:
     impute = KNNImputer()
     x = df[df.index == s].values
     x = impute.fit_transform(x)
     data.loc[data.index == s, 'GPP_NT_VUT_REF'] = x[:,-1]
-    
-data.to_csv('df_imputed.csv')
+
+# Save the cleaned and imputed dataset to a new CSV file    
+data.to_csv('../data/processed/df_imputed.csv')
