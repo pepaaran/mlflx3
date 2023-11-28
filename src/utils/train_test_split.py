25ad8797e8eba766347b2147e2f3d74d617492da
Pepa Aran Paredes <pepa@pop-os.localdomain>
2023-11-28 17:52:44 +0100

    correction for small number os training sites

--- src/utils/train_test_split.py
+++ src/utils/train_test_split.py
@@ -8,7 +8,8 @@ from sklearn.model_selection import train_test_split
 def train_test_split_sites(df):
     """
     Function to split the DataFrame into train and validation sets
-    based on 'TA_F' mean temperature per 'sitename'.
+    based on 'TA_F' mean temperature and 'ai' aridity per 'sitename'.
+    20% of data is used for validation.
 
     Parameters:
     - df (pd.DataFrame): DataFrame containing 'TA_F', 'classid', and 'sitename' columns
@@ -27,22 +28,41 @@ def train_test_split_sites(df):
 
     # Combine discretized columns into a single categorical column for stratification
     grouped['combined_target'] = grouped['TA_F_bins'].astype(str) + '_' + grouped['ai_bins'].astype(str)
+    
+    ## Use train_test_split to create two site groups
 
     # Making sure that there is more than one site in each combined_target group,
     # in order to run train_test_split.
-    # If there is only one site, put it in the training set and split the rest.
     special_sites = grouped.groupby('combined_target').filter(lambda x: len(x) == 1).index.tolist()
-    if len(special_sites):
-        grouped = grouped.drop(special_sites)
-
-    # Use train_test_split to create two site groups, stratified by mean temperature
-    train_df, val_df = train_test_split(grouped, test_size=0.2, stratify=grouped['combined_target'])
     
-    # Get train and validation sites
-    sites_train = train_df.index
-    if len(special_sites):
-        sites_train = sites_train.tolist() + special_sites    # Add weird sites to training set
-    sites_val = val_df.index
+    if len(special_sites) == len(grouped.index.unique()):
+        # If there are few sites for training (e.g. in leave-vegetation-out)
+        # it may lead to all sites being "special", then split at random
+        train_df, val_df = train_test_split(grouped, test_size=0.2)
+
+        # Get train and validation sites
+        sites_train = train_df.index
+        sites_val = val_df.index
+
+    elif len(special_sites):
+        # If there is only one site per category, put it in the training set and split the rest
+        # stratified by mean temperature and aridity.
+        grouped = grouped.drop(special_sites)
+        train_df, val_df = train_test_split(grouped, test_size=0.2, stratify=grouped['combined_target'])
+
+        # Get train and validation sites
+        sites_train = train_df.index.tolist() + special_sites    # Add weird sites to training set
+        sites_val = val_df.index 
+    
+    else:
+        # Use train_test_split to create two site groups, stratified by mean temperature and aridity
+        train_df, val_df = train_test_split(grouped, test_size=0.2, stratify=grouped['combined_target'])
+
+        # Get train and validation sites
+        sites_train = train_df.index
+        sites_val = val_df.index
+
+    print(sites_train)
 
     # Separate the time series data (including the imputed values mask in the last column)
     df_train = df.loc[[any(site == s for s in sites_train) for site in df.index]]
