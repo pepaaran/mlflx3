c764055664b6da5a8f248ae14ae3279d986a1b0c
Pepa Aran Paredes <pepa@pop-os.localdomain>
2023-12-20 15:16:41 +0100

    placeholder for leave-continent-out

---
+++ src/04_lstm_train_leave_continent_out.py
@@ -0,0 +1,6 @@
+# LSTM model with leave-site-out cross-validation on a single continent,
+# This script extracts the data for America and Europe
+# and trains the LSTM model on sites from a given continent at a time.
+
+# TODO: Implement the separation of sites into continents. Beni wanted to keep Australia
+# out of both groups. 
